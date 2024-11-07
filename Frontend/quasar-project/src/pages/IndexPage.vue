389d80eac07c40e679f3f533f0f92a47fe56d829
KatjaBlazevic <katjablaz55@gmail.com>
2024-11-07 12:26:20 +0100

    1. i 2. Zadatak

---
+++ Frontend/quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Knjiznica slika"
+      src="~assets/library.jpg"
+      style="width: 1300px; height: 800px;"
+    />
+  </q-page>
+</template>
+
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
---
+++ Frontend/quasar-project/src/pages/PopisKnjigaPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="popis-knjiga-page">
+    <q-table
+      :rows="books"
+      :columns="columns"
+      row-key="id"
+      :pagination="pagination"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const columns = [
+  {
+    name: 'id',
+    label: 'ID',
+    align: 'left',
+    field: row => row.id,
+    sortable: true
+  },
+  {
+    name: 'title',
+    label: 'Naziv',
+    align: 'left',
+    field: row => row.title,
+    sortable: true
+  },
+  {
+    name: 'author',
+    label: 'Autor',
+    align: 'left',
+    field: row => row.author,
+    sortable: true
+  },
+  {
+    name: 'year',
+    label: 'Godina izdanja',
+    align: 'left',
+    field: row => row.year,
+    sortable: true
+  }
+];
+
+const books = ref([
+  { id: 1, title: 'A Court of Thorns and Roses', author: 'Sarah J. Maas', year: 2021 },
+  { id: 2, title: 'A Court of Mist and Fury', author: 'Sarah J. Maas', year: 2020 },
+  { id: 3, title: 'Never Lie', author: 'Freida McFadden', year: 2019 },
+  { id: 4, title: 'It Ends With Us', author: 'Coleen Hoover', year: 2022 }
+]);
+
+const pagination = ref({
+  page: 1,
+  rowsPerPage: 5
+});
+</script>
+
+<style scoped>
+.popis-knjiga-page {
+  padding: 20px;
+}
+</style>
+
